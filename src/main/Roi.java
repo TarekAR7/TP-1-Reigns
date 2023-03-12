8643acdbcb7f4e3ffb40919c334fade0b1bfa17a
Tine-Henane <114903071+Tine-Henane@users.noreply.github.com>
2023-03-12 22:35:21 +0100

    New classe Roi

--- src/main/Roi.java
+++ src/main/Roi.java
@@ -10,6 +10,7 @@ public class Roi extends Personnage {
     public Roi(String nom) {
         super(nom);
     }
+
     @Override
     public String longRegne(){
         return "Long règne au roi";
