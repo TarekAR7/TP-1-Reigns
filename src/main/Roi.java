58ccadeec3c33d9d91e6a732fa57ec571217ae59
Tine-Henane <114903071+Tine-Henane@users.noreply.github.com>
2023-03-12 22:25:49 +0100

    New classe Roi

---
+++ src/main/Roi.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Roi extends Personnage {
+    /**
+     * Crée un nouveau personnage avec le nom et le genre spécifiés,
+     * puis initialise les jauges de Clergé, Peuple, Armée et Finances.
+     *
+     * @param nom   Le nom du personnage
+     */
+    public Roi(String nom) {
+        super(nom);
+    }
+    @Override
+    public String longRegne(){
+        return "Long règne au roi";
+    }
+}
